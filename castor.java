450ed3a8422acda084f0a453eb0036ba0eae2f01
jeremiassflores <jeremiassflores@gmail.com>
2025-08-21 21:29:19 -0300

    Update castor.java

--- castor.java
+++ castor.java
@@ -1,4 +1,40 @@
 
 public class castor {
     //superclase esta
+
+    private double velocidad;
+    private double longitudDeCola;
+
+    public Castor(double velocidad, double longitudDeCola) {
+        this.velocidad = velocidad;
+        this.longitudDeCola = longitudDeCola;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public double getLongitudDeCola() {
+        return longitudDeCola;
+    }
+
+    public void setLongitudDeCola(double longitudDeCola) {
+        this.longitudDeCola = longitudDeCola;
+    }
+    
+    public void nadar (){
+        System.out.println("Nada a la velidad de " + velocidad + "km/hora");
+        
+    }
+    
+    public void tocarGuitarra (){
+        
+    }
+
+    
 }
+
